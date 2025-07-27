cb15c5436706edac9038c3c1280b000a7ebc5a36
Lakshanan-pro <lakshanan14@gmail.com>
2025-07-27 18:19:17 +0530

    modified app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import sqlite3
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -66,4 +67,5 @@ def delete_transaction(transaction_id):
     return jsonify({"message": "Transaction deleted!"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)

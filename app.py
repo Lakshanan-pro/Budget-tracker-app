d9e63e8dd60e58e33b34ce2f281e88c771a8704c
Lakshanan-pro <lakshanan14@gmail.com>
2025-08-02 23:41:43 +0530

    fixed the port error

--- app.py
+++ app.py
@@ -129,4 +129,7 @@ def transactions():
         return jsonify({'message': 'Transaction deleted'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
